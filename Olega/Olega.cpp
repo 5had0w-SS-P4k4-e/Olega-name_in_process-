d350f76e1a0ab0dbdd67e09329287635358db91b
Mayoness96 <twostoronniy2@gmail.com>
2023-03-07 16:59:32 +0700

    Rename vars

--- Olega/Olega.cpp
+++ Olega/Olega.cpp
@@ -8,36 +8,36 @@
 #include <cmath> 
 using namespace std;
 #define k 3
-#define maxm 100 
-#define minm 50
-//ПОМЕНЯЙ НАЗВАНИЯ ПЕРЕМЕННЫХ!!!
-void FormMatr(float AA[k][k], int nn, int mm)
+#define max_num 100 
+#define min_num 50
+
+void FormMatr(float Matrix[k][k], int nn, int mm)
 {
     int i, j;
     for (i = 0; i < nn; i++) {
         for (j = 0; j < mm; j++)
-            AA[i][j] = rand() % (maxm - minm);
+            Matrix[i][j] = rand() % (max_num - min_num);
     }
 }
 
-float SumMatr(float AA[k][k], int nn, int mm)
+float SumMatr(float Matrix[k][k], int nn, int mm)
 {
     int i, j, sum = 0;
     for (j = 0; j < mm; j++) {
         for (i = 0; i <= j; i++) {
-            sum += AA[i][j];
+            sum += Matrix[i][j];
         };
     };
     return sum;
 }
 
 
-void PrintMatr(float AA[k][k], int nn, int mm)
+void PrintMatr(float Matrix[k][k], int nn, int mm)
 {
     int i, j;
     for (i = 0; i < nn; i++) {
         for (j = 0; j < mm; j++)
-            cout << AA[i][j] << "\t";
+            cout << Matrix[i][j] << "\t";
         cout << endl;
     }
 }
